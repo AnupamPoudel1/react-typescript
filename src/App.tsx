9b7e9915ddc36a06c9beeda133fda7eee62b3a52
Anupam <anupampoudel78@gmail.com>
2024-05-31 16:07:16 +0545

    props and childrens difference learning

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import Section from "./components/Sections"
 
 const App = () => {
   return (
     <div className="bg-black text-white h-screen">
-      <Header title={"Hello World"} />
+      <Header title={"Hello World"} description={"This is just a normal title"} />
+      <Section title="Different Title">This is a new section</Section>
     </div>
   )
 }
---
+++ src/components/Sections.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+
+type SectionProps = {
+    title?: string,
+    children: ReactNode
+}
+
+const Section = ({ children, title = "Section Heading" }: SectionProps) => {
+    return (
+        <section>
+            <h1>
+                {title}
+            </h1>
+            <p>
+                {children}
+            </p>
+        </section>
+    )
+}
+
+export default Section;
