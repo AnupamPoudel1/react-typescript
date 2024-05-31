4244134c6307d893a38406ad7a16389b9d80f673
Anupam <anupampoudel78@gmail.com>
2024-05-31 16:19:36 +0545

    props and passing props through states

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Section from "./components/Sections"
+import Counter from "./components/Counter"
 
 const App = () => {
+
+  const [count, setCount] = useState<number>(0);
+
   return (
     <div className="bg-black text-white h-screen">
       <Header title={"Hello World"} description={"This is just a normal title"} />
       <Section title="Different Title">This is a new section</Section>
+      <Counter setCount={setCount}>The count is {count}</Counter>
     </div>
   )
 }
